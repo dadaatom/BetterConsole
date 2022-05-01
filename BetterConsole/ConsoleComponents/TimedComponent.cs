2eb94ba37eecfc90143723cd0f47861c4282594d
dadaatom <11johnson1111@gmail.com>
2022-05-01 12:56:49 -0400

    Added base time component

---
+++ BetterConsole/ConsoleComponents/TimedComponent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace BetterConsole.ConsoleComponents
+{
+    /*
+     * TODO:
+     * Create some sort of static multithreaded reload for timedcomponents include update ticks.
+     * timer component
+     * countdown component
+     */
+    
+    public abstract class TimedComponent : ConsoleComponent
+    {
+        private DateTime _starting;
+        private DateTime _current;
+        
+        public TimedComponent()
+        {
+            _starting = DateTime.Now;
+            _current = DateTime.Now;
+        }
+
+        public void Reset()
+        {
+            _starting = DateTime.Now;
+            //reset time
+        }
+
+        public void Stop()
+        {
+            // remove this component from timed reload.
+        }
+    }
+}
