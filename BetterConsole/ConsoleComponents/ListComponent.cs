7dbdb28f1ffdf892eb497365bd85e076f2bcef26
dadaatom <11johnson1111@gmail.com>
2022-07-22 00:16:44 -0400

    Base UnorderedListComponent.cs class now works, sublists are are not supported yet.

---
+++ BetterConsole/ConsoleComponents/ListComponent.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+
+namespace BetterConsole.ConsoleComponents
+{
+    /*
+     * TODO:
+     * Sublists shouldn't print with the header.
+     * OrderedLists should changes between styles for sublists.
+     * OrderedLists should include alphabetic ordering as well.
+     */
+    
+    public abstract class ListComponent : ConsoleComponent
+    {
+        public LinkedList<ConsoleComponent> List { get; set; }
+
+        public ListComponent() : this(new ConsoleComponent[]{}){ }
+
+        public ListComponent(string[] list)
+        {
+            List = new LinkedList<ConsoleComponent>();
+            foreach (string str in list)
+            {
+                List.AddLast(new TextComponent(str));
+            }
+        }
+
+        public ListComponent(ConsoleComponent[] list)
+        {
+            List = new LinkedList<ConsoleComponent>();
+            foreach (ConsoleComponent component in list)
+            {
+                List.AddLast(component);
+            }
+        }
+
+        public abstract override string ToString();
+
+        /// <summary>
+        /// Tabs a string for a list element.
+        /// </summary>
+        /// <param name="component">Component to add tabbing.</param>
+        /// <param name="header">Header added in front of component.</param>
+        /// <param name="paddedHeader">Padded header added in front of multiline component strings.</param>
+        /// <returns>Tabbed component with a header in front of the first line.</returns>
+        protected string TabComponent(ConsoleComponent component, string header, string paddedHeader = null)
+        {
+            string toReturn = "";
+
+            if (paddedHeader == null)
+            {
+                paddedHeader = "";
+                for (int i = 0; i < header.Length; i++)
+                {
+                    paddedHeader += " ";
+                }
+            }
+
+            string[] lines = component.ToString().Split('\n');
+            for (int i = 0; i < lines.Length; i++) {
+                if (i == 0)
+                {
+                    toReturn += header + lines[i];
+                }
+                else
+                {
+                    toReturn += paddedHeader + lines[i];
+                }
+
+                if (i < lines.Length - 1)
+                {
+                    toReturn += "\n";
+                }
+            }
+
+            return toReturn;
+        }
+
+        /// <summary>
+        /// Adds text component to the console component list.
+        /// </summary>
+        /// <param name="str">String to be added to the component list.</param>
+        public void AddTextToList(string str)
+        {
+            List.AddLast(new TextComponent(str));
+        }
+    }
+}
---
+++ BetterConsole/ConsoleComponents/UnorderedListComponent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BetterConsole.ConsoleComponents
+{
+    public class UnorderedListComponent : ListComponent
+    {
+        private string _itemHeader = " - ";
+        
+        public UnorderedListComponent() { }
+
+        public UnorderedListComponent(string[] list) : base(list) { }
+
+        public UnorderedListComponent(ConsoleComponent[] list) : base(list) { }
+
+        public override string ToString()
+        {
+            string toReturn = "";
+
+            string paddedHeader = "";
+            for (int i = 0; i < _itemHeader.Length; i++)
+            {
+                paddedHeader += " ";
+            }
+
+            int counter = 0;
+            foreach (ConsoleComponent component in List)
+            {
+                toReturn += TabComponent(component, _itemHeader, paddedHeader);
+                
+                if (counter < List.Count - 1)
+                {
+                    toReturn += "\n";
+                }
+
+                counter++;
+            }
+
+            return toReturn;
+        }
+    }
+}
