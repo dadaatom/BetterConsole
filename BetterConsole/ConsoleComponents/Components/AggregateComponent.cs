41290f99cc4e8110631ec61d611c56f872f354bf
dadaatom <11johnson1111@gmail.com>
2022-08-20 21:37:32 -0400

    AggregateComponent.cs added.

---
+++ BetterConsole/ConsoleComponents/Components/AggregateComponent.cs
@@ -0,0 +1,22 @@
+﻿namespace BetterConsole.ConsoleComponents
+{
+    public class AggregateComponent : ConsoleComponent
+    {
+        public ConsoleComponent[] Components;
+        
+        public AggregateComponent(ConsoleComponent[] components)
+        {
+            Components = components;
+        }
+
+        public override string ToString()
+        {
+            string toReturn = "";
+            foreach (ConsoleComponent component in Components)
+            {
+                toReturn += component.ToString();
+            }
+            return toReturn;
+        }
+    }
+}
